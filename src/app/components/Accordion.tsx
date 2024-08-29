ea4ea390c2027554dfe589523cd681d17eac40f9
bipolab-eu <bentagai@bipolab.eu>
2024-08-29 10:39:19 +0100

    updates

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import styled from 'styled-components'
 
@@ -6,37 +8,34 @@ interface Props {
   description: string;
 }
 
-
 const StyledDetails = styled.details`
   width: 100%;
   background: #F9FAFB;
   color: #111827;
+  border-radius: 1rem;
 
   &[open] summary::after {
     content: url('data:image/svg+xml,%3Csvg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"%3E%3Cpath d="M4.5 15.75L12 8.25L19.5 15.75" stroke="%23111827" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/%3E%3C/svg%3E');
-    font-size: 1.5rem;
+    line-height: 0%;
   }
 
-  summary {
+  .accordion-title {
     padding: 1rem;
-    border-bottom: 1px solid #111827;
     cursor: pointer;
     list-style-type: none;
     display: flex;
+    align-items: center;
     justify-content: space-between;
+    font-size: 1rem;
 
     &::after {
       content: url('data:image/svg+xml,%3Csvg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"%3E%3Cpath d="M19.5 8.25L12 15.75L4.5 8.25" stroke="%23111827" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/%3E%3C/svg%3E');
-      font-size: 1.5rem;
-    }
-
-    &:hover {
-      background-color: #9f9fa1;
+      line-height: 0%;
     }
   }
 
-  p {
-    padding: 1rem;
+  .accordion-content {
+    padding: 1rem 1rem 2rem 1rem;
     font-size: 1rem;
     font-style: normal;
     font-weight: 400;
@@ -45,8 +44,8 @@ const StyledDetails = styled.details`
 `;
 
 export const Accordion: React.FC<Props> = ({ title, description }) => (
-  <StyledDetails name="reqs">
-    <summary>{title}</summary>
-    <p>{description}</p>
+  <StyledDetails name="accordion">
+    <summary className='accordion-title'>{title}</summary>
+    <p className='accordion-content'>{description}</p>
   </StyledDetails>
 )
