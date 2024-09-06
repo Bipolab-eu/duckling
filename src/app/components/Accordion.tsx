9c6ec1fc4b76f44c241310d9925322cd8a5d3266
bipolab-eu <bentagai@bipolab.eu>
2024-09-06 12:38:36 +0100

    mejoras en accordion

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -2,11 +2,15 @@
 
 import React from 'react'
 import styled from 'styled-components'
+import test from '../icons/test.svg'
 
 interface Props {
   title: string;
   description: string;
 }
+
+const chevronDown = 'data:image/svg+xml,%3Csvg%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20xmlns%3D%22http://www.w3.org/2000/svg%22%3E%3Cpath%20d%3D%22M19.5%208.25L12%2015.75L4.5%208.25%22%20stroke%3D%22%23111827%22%20stroke-width%3D%221.5%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22/%3E%3C/svg%3E';
+const chevronUp = 'data:image/svg+xml,%3Csvg%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20xmlns%3D%22http://www.w3.org/2000/svg%22%3E%3Cpath%20d%3D%22M4.5%2015.75L12%208.25L19.5%2015.75%22%20stroke%3D%22%23111827%22%20stroke-width%3D%221.5%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22/%3E%3C/svg%3E';
 
 const StyledDetails = styled.details`
   width: 100%;
@@ -15,7 +19,7 @@ const StyledDetails = styled.details`
   border-radius: 1rem;
 
   &[open] summary::after {
-    content: url('data:image/svg+xml,%3Csvg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"%3E%3Cpath d="M4.5 15.75L12 8.25L19.5 15.75" stroke="%23111827" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/%3E%3C/svg%3E');
+    content: url(${chevronUp});
     line-height: 0%;
   }
 
@@ -29,7 +33,7 @@ const StyledDetails = styled.details`
     font-size: 1rem;
 
     &::after {
-      content: url('data:image/svg+xml,%3Csvg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"%3E%3Cpath d="M19.5 8.25L12 15.75L4.5 8.25" stroke="%23111827" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/%3E%3C/svg%3E');
+      content: url(${chevronDown});
       line-height: 0%;
     }
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Accordion } from './components/Accordion'
 
 export default function Home() {
   return (
       /* Importa el componente dentro del contenedor lab */
     <div id='lab'>
-
+<Accordion title='hola mundo' description='adios mundo'></Accordion>
     </div>
   )
 }
