405c494db202cb4c83c164c25bfd6ef4b6ff69bf
bipolab-eu <bentagai@bipolab.eu>
2024-10-23 15:57:47 +0100

    mejoras en componente button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,24 @@
 
 import React from 'react'
 
+const styles = {
+  base: 'border border-gray-900 p-4 text-sm text-center leading-none rounded-full inline-block min-w-32 w-fit',
+
+  variants: {
+    primary: 'bg-gray-50 text-gray-900 hover:bg-gray-900 hover:text-gray-50',
+    secondary: 'bg-gray-900 text-gray-50 hover:bg-gray-50 hover:text-gray-900'
+  }
+}
+
 interface Props {
   label: string,
-  variant: 'btn-primary' | 'btn-secondary',
+  variant: keyof typeof styles.variants,
   onPress: () => void
 }
 
 export const Button: React.FC<Props> = ({ label, variant, onPress }) => (
   <button
-    className={variant}
+    className={`${styles.base} ${styles.variants[variant]}`}
     onClick={onPress}
   >
     {label}
