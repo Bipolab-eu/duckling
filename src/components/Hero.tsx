2ab587d7451fa373aafe497c24b23f303cec0bee
bipolab-eu <bentagai@bipolab.eu>
2024-10-29 13:10:22 +0000

    mejoras en componente Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,32 @@
-'use client'
-
 import React from 'react'
 import { Button } from './Button'
 
-interface Props {
-  title: string,
-  subtitle: string
+const { backgroundColor, textAlignment, heroTitle, heroSubtitle, shapes } = {
+  backgroundColor: 'bg-gradient-to-b from-neutral-950 to-neutral-500',
+  textAlignment: 'items-center text-center text-neutral-50',
+  heroTitle:    'text-2xl md:text-4xl font-extrabold',
+  heroSubtitle: 'text-lg  md:text-2xl font-medium',
+
+  shapes: {
+    squared: 'rounded-none',
+    rounded: 'rounded-2xl',
+  },
 }
 
-export const Hero: React.FC<Props> = ({ title, subtitle }) => {
-  function foo() {
-    return console.log('hola mundo')
-  }
+interface Props {
+  title: string,
+  subtitle: string,
+  shape: keyof typeof shapes,
+}
 
+export const Hero: React.FC<Props> = ({ title, subtitle, shape }) => {
   return (
-    <header className='p-4 md:p-0 rounded-2xl bg-pink-700 flex justify-center items-center gap-4 h-96'>
-      <section className='md:w-1/2 flex flex-col gap-4 items-center text-center text-gray-50'>
-        <h1 className='text-lg md:text-4xl font-semibold'>{title}</h1>
-        <h2 className='md:text-xl'>{subtitle}</h2>
-        <Button label="Dona" variant="primary" shape="rounded" onPress={foo} />
+    <header className={`flex flex-col justify-center h-96 gap-8 p-4 ${backgroundColor} ${textAlignment} ${shapes[shape]}`}>
+      <section className={`grid gap-2 md:w-2/4`}>
+        <h1 className={`${heroTitle}`}>{title}</h1>
+        <h2 className={`${heroSubtitle}`}>{subtitle}</h2>
       </section>
+      <Button label='Button Label' shape='squared' variant='secondary' size='normal' />
     </header>
   )
 }
